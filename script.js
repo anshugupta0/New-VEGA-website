424ae00e82f73d39b95eb7ee8451394c375f6d7c
Anshu Gupta <guptaanshu508@gmail.com>
2021-11-14 20:15:20 +0530

    Adding all the files

---
+++ script.js
@@ -0,0 +1,6 @@
+// const hamburger = document.getElementById('humburger');
+// const navUL = document.getElementById('nav-links');
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     navUL.addEventListener('click', false);
+// });
